101cbde08643db2de52648d8f5b3ec81eb1a3c42
Kosench <dimako36@mail.ru>
2025-03-01 21:30:28 +1000

    feat: implement JWT token verification logic

--- token/jwt_maker.go
+++ token/jwt_maker.go
@@ -1,6 +1,7 @@
 package token
 
 import (
+	"errors"
 	"fmt"
 	"github.com/golang-jwt/jwt"
 	"time"
@@ -21,8 +22,29 @@ func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (str
 }
 
 func (maker *JWTMaker) VerifyToken(token string) (*Payload, error) {
-	//TODO implement me
-	panic("implement me")
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, ErrInvalidToken
+		}
+		return []byte(maker.secretKey), nil
+	}
+
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, keyFunc)
+	if err != nil {
+		verr, ok := err.(*jwt.ValidationError)
+		if ok && errors.Is(verr.Inner, ErrExpiredToken) {
+			return nil, ErrExpiredToken
+		}
+		return nil, ErrInvalidToken
+	}
+
+	payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+
+	return payload, nil
 }
 
 const minSecretKeySize = 32
