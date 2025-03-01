2a44c2617f784fcf41a9dcc4d1c56f678dd204f5
Kosench <dimako36@mail.ru>
2025-03-01 22:51:47 +1000

    feat(auth): add PASETO support with shared Maker interface

---
+++ token/paseto_maker.go
@@ -0,0 +1,36 @@
+package token
+
+import (
+	"fmt"
+	"github.com/o1egl/paseto"
+	"golang.org/x/crypto/chacha20poly1305"
+	"time"
+)
+
+type PasetoMaker struct {
+	paseto       *paseto.V2
+	symmetricKey []byte
+}
+
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewPasetoMaker(symmetricKey string) (Maker, error) {
+	if len(symmetricKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("invalid key size: must be exactly %d characters", chacha20poly1305.KeySize)
+	}
+
+	maker := &PasetoMaker{
+		paseto:       paseto.NewV2(),
+		symmetricKey: []byte(symmetricKey),
+	}
+
+	return maker, nil
+}
