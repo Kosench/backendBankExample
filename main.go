6dc66a1e376e149c044b96870cc97ae83a3a4b7e
Kosench <dimako36@mail.ru>
2025-03-17 22:07:17 +1000

    feat: add statik-based Swagger file serving

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
 	"database/sql"
 	"github.com/Kosench/backendBankExample/api"
 	db "github.com/Kosench/backendBankExample/db/sqlc"
+	_ "github.com/Kosench/backendBankExample/doc/statik"
 	"github.com/Kosench/backendBankExample/gapi"
 	"github.com/Kosench/backendBankExample/pb"
 	"github.com/Kosench/backendBankExample/util"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
+	"github.com/rakyll/statik/fs"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -98,8 +100,12 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
-	fs := http.FileServer(http.Dir("./doc/swagger"))
-	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal("cannot create statik fs:", err)
+	}
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	mux.Handle("/swagger/", swaggerHandler)
 
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
@@ -110,6 +116,6 @@ func runGatewayServer(config util.Config, store db.Store) {
 
 	err = http.Serve(listener, mux)
 	if err != nil {
-		log.Fatal("cannot start HTTP gateway server")
+		log.Fatal("cannot start HTTP gateway server", err)
 	}
 }
